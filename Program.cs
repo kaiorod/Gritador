4d20612ad119d5cec95ce515ae70423bb4ee47c1
kaiorod <caiorodrigo2775@gmail.com>
2021-08-20 23:37:07 -0300

    finaliza Gritador

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.Clear();
+            Console.Beep();
+
+            Console.WriteLine("Olá usuário, irei GRITAR tudo que você digitiar!");
+
+            Console.Write("Digite algo : ");
+            texto = Console.ReadLine();
+
+            Console.Beep();
+            Console.WriteLine($"\nVocê Digitou : ");
+            Console.ForegroundColor = ConsoleColor.Cyan; 
+            Console.WriteLine(texto);
+            Console.ResetColor();
+
+            Console.WriteLine($"GRITADO fica : ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(texto.ToUpper());
+            Console.ResetColor();
+
+
+
+        }
+    }
+}
